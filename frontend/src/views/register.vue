cbc1cd0d1ebc758ba916a455b309cdefc40f2cb0
BENJY33X <92626379+BENJY33X@users.noreply.github.com>
2022-04-13 04:58:27 +0000

    register

---
+++ frontend/src/views/register.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <register/>
+    </div>
+</template>
+
+
+
+
+<script>
+import register from '../components/register.vue'
+export default {
+    name:"Sing-in",
+    components:{register}
+    
+}
+</script>
