e4494bbaae3fc8bb81cb1e89a7e7c4385f682fd3
BENJY33X <92626379+BENJY33X@users.noreply.github.com>
2022-04-13 04:57:57 +0000

    views index

---
+++ frontend/src/views/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <!-- <Navbar /> -->
+    </div>
+</template>
+
+
+
+
+
+<script>
+// import Navbar from '../components/navbar.vue'
+import '../../src/index.css'
+export default {
+    name: "H-ome",
+    // components: { Navbar }
+}
+</script>
+
+
+
+<style>
+</style>
