120be9aea156c7044c21dd13f8bd6a6c87ce15da
BENJY33X <92626379+BENJY33X@users.noreply.github.com>
2022-04-13 04:58:18 +0000

    component login

---
+++ frontend/src/views/login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <Login />
+    </div>
+</template>
+
+
+
+<script>
+import Login from '../components/login.vue'
+export default {
+    name:"log-in",
+    components:{Login}
+}
+</script>
