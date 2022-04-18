54fa34385c42b5fbfa845fb7f4767d99bbe2d65e
BENJY33X <92626379+BENJY33X@users.noreply.github.com>
2022-04-18 23:02:20 +0000

    affichage des offre

--- frontend/src/components/Offre/afficher.vue
+++ frontend/src/components/Offre/afficher.vue
@@ -1,115 +1,122 @@
 <template>
-        <div class="h-full bg-gradient-to-tr from-gray-300 to-gray-200 flex justify-center items-center py-20">
-            <div class="md:px-4 md:grid md:grid-cols-2 lg:grid-cols-3 gap-5 space-y-4 md:space-y-0">
-                <div
-                    class="max-w-sm bg-white px-6 pt-6 pb-2 rounded-xl shadow-lg transform hover:scale-105 transition duration-500">
-                    <!-- nom de client qui post cette post -->
-                    <h3 class="mb-3 text-xl font-bold text-indigo-600">Abdessalam Staili</h3>
-                    <div class="relative">
-                        <!-- image de chause qui post -->
-                        <img class="w-full rounded-xl" src="../../assets/back.png" alt="Colors" />
-                        <!-- prix de site pieces -->
-                        <p
-                            class="absolute top-0 bg-yellow-300 text-gray-800 font-semibold py-1 px-3 rounded-br-lg rounded-tl-lg">
-                            $0</p>
-                    </div>
-                    <!-- description -->
-                    <h4 class="mt-4 text-gray-800 text-2xl font-bold cursor-pointer">cette camera et tres bonne qualiter
-                        60px</h4>
-                    <div class="my-4">
-                        <div class="flex space-x-1 items-center">
-                            <span>
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-indigo-600 mb-1.5"
-                                    fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                </svg>
-                            </span>
-                            <!-- time de post -->
-                            <p>1:34:23 Minutes</p>
-                        </div>
-                        <div class="flex space-x-1 items-center">
-                            <span>
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-indigo-600 mb-1.5"
-                                    fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                                </svg>
-                            </span>
-                            <!-- type demande or offre -->
-                            <p>Offre</p>
-                        </div>
-                        <div class="flex space-x-1 items-center">
-                            <span>
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-indigo-600 mb-1.5"
-                                    fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
-                                </svg>
-                            </span>
-                            <p>nouveau</p>
-                        </div>
-                        <button class="mt-4 text-xl w-full text-white bg-gray-600 py-2 rounded-xl shadow-lg">Buy
-                            Lesson</button>
-                    </div>
+    <div class="h-full bg-gradient-to-tr from-gray-300 to-gray-200 flex justify-center items-center py-20">
+        <div v-for="post in posts" :key="post.id" class="md:px-4 md:grid md:grid-cols-2 lg:grid-cols-3 gap-5 space-y-4 md:space-y-0">
+            <div
+                class="max-w-sm min-w-sm bg-white px-6 pt-9 pb-2 rounded-xl shadow-lg transform hover:scale-105 transition duration-500">
+                <!-- nom de client qui post cette post -->
+                <h3 class="mb-3 text-xl font-bold text-indigo-600">Abdessalam Staili</h3>
+                <div class="relative">
+                    <!-- image de chause qui post -->
+                    <img class="w-full rounded-xl" src="../../assets/back.png" alt="Colors" />
+                    <!-- prix de site pieces -->
+                    <p
+                        class="absolute top-0 bg-yellow-300 text-gray-800 font-semibold py-1 px-3 rounded-br-lg rounded-tl-lg">{{post.Prix}}$</p>
                 </div>
-                <div
-                    class="max-w-sm bg-white px-6 pt-6 pb-2 rounded-xl shadow-lg transform hover:scale-105 transition duration-500">
-                    <h3 class="mb-3 text-xl font-bold text-indigo-600">Beginner Friendly</h3>
-                    <div class="relative">
-                        <img class="w-full rounded-xl"
-                            src="https://images.unsplash.com/photo-1561835491-ed2567d96913?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80"
-                            alt="Colors" />
-                        <p
-                            class="absolute top-0 bg-yellow-300 text-gray-800 font-semibold py-1 px-3 rounded-br-lg rounded-tl-lg">
-                            $50</p>
+                <!-- description -->
+                <h4 class="mt-4 text-gray-800 text-2xl font-bold cursor-pointer">{{post.Title}}: <br> <span class="font-normal">{{post.description}}</span> </h4>
+                <div class="my-4">
+                    <div class="flex space-x-1 items-center">
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-indigo-600 mb-1.5" fill="none"
+                                viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
+                        </span>
+                        <!-- time de post -->
+                        <p>1:34:23 Minutes</p>
                     </div>
-                    <h1 class="mt-4 text-gray-800 text-2xl font-bold cursor-pointer">Advanced React Native for
-                        Sustainability</h1>
-                    <div class="my-4">
-                        <div class="flex space-x-1 items-center">
-                            <span>
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-indigo-600 mb-1.5"
-                                    fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                </svg>
-                            </span>
-                            <p>1:34:23 Minutes</p>
-                        </div>
-                        <div class="flex space-x-1 items-center">
-                            <span>
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-indigo-600 mb-1.5"
-                                    fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                                </svg>
-                            </span>
-                            <p>3 Parts</p>
-                        </div>
-                        <div class="flex space-x-1 items-center">
-                            <span>
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-indigo-600 mb-1.5"
-                                    fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
-                                </svg>
-                            </span>
-                            <p>Vanilla JS</p>
-                        </div>
-                        <button class="mt-4 text-xl w-full text-white bg-gray-600 py-2 rounded-xl shadow-lg">Buy
-                            Lesson</button>
+                    <div class="flex space-x-1 items-center">
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-indigo-600 mb-1.5" fill="none"
+                                viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                            </svg>
+                        </span>
+                        <!-- type demande or offre -->
+                        <p>{{post.Type}}</p>
                     </div>
+                    <!-- <div class="flex space-x-1 items-center">
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-indigo-600 mb-1.5" fill="none"
+                                viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
+                            </svg>
+                        </span> -->
+                        <!-- <p>nouveau</p> -->
+                    <!-- </div> -->
+                    <button class="mt-4 text-xl w-full text-white bg-gray-600 py-2 rounded-xl shadow-lg">Buy
+                        Lesson</button>
                 </div>
             </div>
         </div>
+
+
+
+
+        <div @click="affich" class="fixed right-1 top-20">
+            <div class="m-3">
+                <button
+                    class="bg-white text-gray-800 font-bold rounded border-b-2 border-green-500 hover:border-green-600 hover:bg-green-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
+                    <span class="mr-2">Add Post</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path fill="currentcolor" d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"></path>
+                    </svg>
+                </button>
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+        <!-- <button  class="p-2 pl-5 pr-5 bg-transparent border-2 border-gray-500 text-gray-500 text-lg rounded-lg hover:bg-gray-500 hover:text-gray-100 focus:border-4 focus:border-gray-300">Secondary</button> -->
+        <Add @close="fermer" v-if="close" class="form-fixed" />
+    </div>
 </template>
 
 <script>
+import axios from 'axios';
+import Add from "./addOffre.vue";
 export default {
-name:"affich-offre"
+    name: "affich-offre",
+    components: {
+        Add
+    },
+    data() {
+        return {
+            close: false,
+            posts: "",
+        }
+    },
+    methods: {
+        affich() {
+            this.close = true;
+        },
+        fermer() {
+            this.close = false;
+        },
+        getAllPost() {
+            axios.get(" http://127.0.0.1:8000/api/getAllPost").then(res => {
+                this.posts = res.data
+            });
+        },
+    },
+    mounted() {
+        this.getAllPost();
+    },
 }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+.form-fixed {
+    position: fixed;
+    top: -86px;
+    right: 0;
+}
 </style>
