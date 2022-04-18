45266e61846e6d56701d9826153560045965793e
BENJY33X <92626379+BENJY33X@users.noreply.github.com>
2022-04-18 23:01:43 +0000

    modification app vuejs

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -13,10 +13,31 @@
 
 import Nav from "./components/NavFoter/navbar.vue"
 import Footer from "./components/NavFoter/footer.vue"
+import { computed } from "@vue/reactivity"
 
 export default {
   components: {  Nav, Footer },
   name: 'App',
+  data() {
+    return {
+            isLogin: false,
+    }
+  },
+  methods: {
+     setLogin(login) {
+      this.isLogin = login
+    },
+  },
+    provide() {
+    return {
+      isLogin: computed(() => this.isLogin),
+      setLogin: this.setLogin
+
+    }
+  },
+  mounted() {
+    this.isLogin = !!localStorage.getItem("token")
+  },
 
 }
 </script>
