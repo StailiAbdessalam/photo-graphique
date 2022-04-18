28b356ccf02d1af39504d4cf356af9f01ed85b22
BENJY33X <92626379+BENJY33X@users.noreply.github.com>
2022-04-18 23:02:11 +0000

    add offre

---
+++ frontend/src/components/Offre/addOffre.vue
@@ -0,0 +1,174 @@
+<template>
+    <div>
+        <div class="">
+            <section id="login" class="p-4 flex flex-col justify-center min-h-screen max-w-md mx-auto">
+                <div class="p-6 bg-gray-600 rounded">
+                    <div class="flex items-center justify-center font-black m-3 mb-12">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 mr-3 text-red-600 animate-pulse"
+                            viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd"
+                                d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+                                clip-rule="evenodd" />
+                        </svg>
+                        <h1 class="tracking-wide text-3xl text-gray-900">POSTER</h1>
+                        <div @click="$emit('close',false)" class="m-3">
+                            <button
+                                class="bg-white text-gray-800 font-bold rounded border-b-2 border-red-500 hover:border-red-600 hover:bg-red-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
+                                <span class="mr-2">close</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                    <path fill="currentcolor"
+                                        d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" />
+                                </svg>
+                            </button>
+                        </div>
+
+                    </div>
+                    <form id="login_form" @click="subsub" action="api_login" method="POST"
+                        class="flex flex-col justify-center">
+                        <label class=" m-2 text-white font-medium">image</label>
+                        <div class="container mx-auto h-full flex flex-col justify-center items-center">
+                            <input @change="processFile($event)" type="file" id="multi-upload-input" multiple />
+                        </div>
+                        <div class="col-span-6 sm:col-span-4">
+                            <label class=" m-2 block text-white font-medium text-gray-700">Type</label>
+                            <select @change="typefun" v-model="newPost.Type" id="vehicle_id" name="vehicle_id"
+                                class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                <!-- <option >-- select Type no --</option> -->
+                                <option value="Offre" data-val="1">Offre</option>
+                                <option value="demande" data-val="2">demande</option>
+                            </select>
+                        </div>
+                        <label v-if="offre" class=" m-2 block text-white font-medium text-gray-700">entrer your
+                            Prix</label>
+                        <div v-if="offre" class="flex justify-between items-center mb-3">
+                            <div class="inline-flex items-center self-start">
+                                <svg xmlns="http://www.w3.org/2000/svg"
+                                    class="h-8 w-8 mr-3 bg-gradient-to-r from-pink-600 to-red-600 shadow-lg rounded p-1.5 text-gray-100"
+                                    viewBox="0 0 20 20" fill="currentColor">
+                                    <path d="M13 7H7v6h6V7z" />
+                                    <path fill-rule="evenodd"
+                                        d="M7 2a1 1 0 012 0v1h2V2a1 1 0 112 0v1h2a2 2 0 012 2v2h1a1 1 0 110 2h-1v2h1a1 1 0 110 2h-1v2a2 2 0 01-2 2h-2v1a1 1 0 11-2 0v-1H9v1a1 1 0 11-2 0v-1H5a2 2 0 01-2-2v-2H2a1 1 0 110-2h1V9H2a1 1 0 010-2h1V5a2 2 0 012-2h2V2zM5 5h10v10H5V5z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                                <span class="font-bold text-gray-900">$5 / Prix</span>
+                            </div>
+                            <div class="flex">
+                                <button type="button" @click="minus()" class="bg-yellow-600 p-1.5 font-bold rounded">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"
+                                        fill="currentColor">
+                                        <path fill-rule="evenodd" d="M5 10a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1z"
+                                            clip-rule="evenodd" />
+                                    </svg>
+                                </button>
+
+                                <input v-model="newPost.Prix" id="item_count" type="number" class="max-w-[100px] font-bold font-mono py-1.5 px-2 mx-1.5
+            block border border-gray-300 rounded-md text-sm shadow-sm  placeholder-gray-400
+            focus:outline-none
+            focus:border-sky-500
+            focus:ring-1
+            focus:ring-sky-500
+            focus:invalid:border-red-500  focus:invalid:ring-red-500">
+                                <button type="button" @click="plus()" class="bg-green-600 p-1.5 font-bold rounded">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"
+                                        fill="currentColor">
+                                        <path fill-rule="evenodd"
+                                            d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                                            clip-rule="evenodd" />
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                        <label class="text-white font-medium">title</label>
+                        <input v-model="newPost.Title" class="mb-3 px-2 py-1.5
+                        
+          mb-3 mt-1 block w-full px-2 py-1.5 border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400
+          focus:outline-none
+          focus:border-sky-500
+          focus:ring-1
+          focus:ring-sky-500
+          focus:invalid:border-red-500 focus:invalid:ring-red-500" type="text" name="username" placeholder="wahyusa">
+
+                        <label class="text-white font-medium">description</label>
+                        <textarea class="
+          mb-3 mt-1 block w-full px-2 py-1.5 border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400
+          focus:outline-none
+          focus:border-sky-500
+          focus:ring-1
+          focus:ring-sky-500
+          focus:invalid:border-red-500 focus:invalid:ring-red-500" name="messages"
+                            placeholder="Write something"></textarea>
+                        <button
+                            class="px-4 py-1.5 rounded-md shadow-lg bg-gradient-to-r from-pink-600 to-red-600 font-medium text-gray-100 block transition duration-300"
+                            type="submit">
+                            <span id="login_process_state" class="hidden">Sending :)</span>
+                            <span id="login_default_state">Donate<span id="subtotal"></span></span>
+                        </button>
+                    </form>
+                </div>
+            </section>
+        </div>
+    </div>
+</template>
+
+
+
+<script>
+export default {
+    name: "add-item",
+    methods: {
+        processFile(event) {
+            this.newPost.pucture = event.target.files[0]
+        },
+        plus() {
+            let count = parseInt(document.getElementById('item_count').value);
+            document.getElementById('item_count').value = count + 1;
+            this.subtotal = count + 1;
+        },
+        minus() {
+            let count = parseInt(document.getElementById('item_count').value);
+            if (count > 1) {
+                document.getElementById('item_count').value = count - 1;
+                this.subtotal = count - 1;
+            }
+        },
+        subsub() {
+            document.getElementById("login_form").onsubmit = function () {
+                event.preventDefault()
+                // animation
+                document.getElementById("login_process_state").classList.remove("hidden")
+                document.getElementById("login_process_state").classList.add("animate-pulse")
+
+                document.getElementById("login_default_state").classList.add("hidden")
+            }
+
+            parseInt(document.getElementById("item_count").value)
+            this.subtotal = parseInt(5)
+        },
+        typefun() {
+            document.getElementById("vehicle_id").value == "Offre" ? this.offre = true : this.offre = false
+        },
+
+    },
+    data() {
+        return {
+            newPost: {
+                Prix: 1,
+                Title: '',
+                pucture: '',
+                Type: '',
+            },
+            offre: false,
+
+
+        }
+    },
+
+
+
+}
+
+
+
+
+
+</script>
