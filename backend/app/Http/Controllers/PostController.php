ccc1634dbcb1e9074d4007693c0bcb663b5a0adc
BENJY33X <92626379+BENJY33X@users.noreply.github.com>
2022-04-19 14:46:07 +0000

    add post controller

--- backend/app/Http/Controllers/PostController.php
+++ backend/app/Http/Controllers/PostController.php
@@ -4,11 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Symfony\Component\VarDumper\VarDumper;
 
 class PostController extends Controller
 {
     public function getAll()
     {
         return  json_encode(Post::all());
+    }
+
+
+    public function Add(Request $request)
+    {
+
+            $Post = new Post();
+            $Post->user_id = $request->user_id;
+            $Post->Title = $request->Title;
+            $Post->image = $request->image;
+            $Post->Prix = $request->Prix;
+            $Post->Type = $request->Type;
+            $Post->description = $request->description;
+            $result = $Post->save();
+            echo json_encode($result);
+            // return Post::create($request->all());
+
     }
 }
