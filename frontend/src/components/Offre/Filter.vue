a7eb44a6630016215cd40136d0c23dff1a468764
BENJY33X <92626379+BENJY33X@users.noreply.github.com>
2022-04-19 14:47:17 +0000

    filter

---
+++ frontend/src/components/Offre/Filter.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <div class="my-2  flex sm:flex-row flex-col">
+            <div class="flex  flex-row mb-1 sm:mb-0">
+
+                <div class="relative">
+                    <select
+                        class="appearance-none h-full rounded-r border-t sm:rounded-r-none sm:border-r-0 border-r rounded-md border-b block appearance-none w-full bg-yellow-400 border-gray-400 text-black py-2 px-4 pr-8 leading-tight focus:outline-none focus:border-l focus:border-r focus:bg-white focus:border-gray-500">
+                        <option>All</option>
+                        <option>Offre</option>
+                        <option>demande</option>
+                    </select>
+                    <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                        </svg>
+                    </div>
+                </div>
+            </div>
+            <!-- <div class="block relative">
+                <span class="h-full absolute inset-y-0 left-0 flex items-center pl-2">
+                    <svg viewBox="0 0 24 24" class="h-4 w-4 fill-current text-gray-500">
+                        <path
+                            d="M10 4a6 6 0 100 12 6 6 0 000-12zm-8 6a8 8 0 1114.32 4.906l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387A8 8 0 012 10z">
+                        </path>
+                    </svg>
+                </span>
+                <input placeholder="Search"
+                    class="appearance-none rounded-r rounded-l sm:rounded-l-none border border-gray-400 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none" />
+            </div> -->
+        </div>
+
+
+    </div>
+</template>
+
+<script>
+export default {
+    name:"filter-ter"
+
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
