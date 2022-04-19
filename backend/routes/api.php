9fb3716f2b9de917ddc48e255b2ca71e31b2fb4c
BENJY33X <92626379+BENJY33X@users.noreply.github.com>
2022-04-19 14:46:25 +0000

    api add post

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -23,3 +23,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
  Route::post('Login',[AuthController::class,'Login']);
  Route::post('register',[AuthController::class,'checkEmail']);
  Route::get('getAllPost',[PostController::class,'getAll']);
+ Route::post('AddPost',[PostController::class,'Add']);
