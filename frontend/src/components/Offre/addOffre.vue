d45c9ec624b01dd78e6791f4cf56c0ef70456f1b
BENJY33X <92626379+BENJY33X@users.noreply.github.com>
2022-04-19 14:47:07 +0000

    add offre

--- frontend/src/components/Offre/addOffre.vue
+++ frontend/src/components/Offre/addOffre.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <div class="">
+        <div class=" bg-opacity-70 bg-black w-screen ">
             <section id="login" class="p-4 flex flex-col justify-center min-h-screen max-w-md mx-auto">
-                <div class="p-6 bg-gray-600 rounded">
+                <div class="p-6 bg-gray-700 rounded">
                     <div class="flex items-center justify-center font-black m-3 mb-12">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 mr-3 text-red-600 animate-pulse"
                             viewBox="0 0 20 20" fill="currentColor">
@@ -10,10 +10,10 @@
                                 d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
                                 clip-rule="evenodd" />
                         </svg>
-                        <h1 class="tracking-wide text-3xl text-gray-900">POSTER</h1>
-                        <div @click="$emit('close',false)" class="m-3">
+                        <h1 class="tracking-wide text-3xl text-white">POSTER</h1>
+                        <div @click="$emit('close', false)" class="m-3">
                             <button
-                                class="bg-white text-gray-800 font-bold rounded border-b-2 border-red-500 hover:border-red-600 hover:bg-red-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
+                                class="bg-red-500 text-white font-bold rounded border-b-2 border-red-500 hover:border-red-600 hover:bg-white hover:text-black shadow-md py-2 px-6 inline-flex items-center">
                                 <span class="mr-2">close</span>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                                     <path fill="currentcolor"
@@ -23,11 +23,12 @@
                         </div>
 
                     </div>
-                    <form id="login_form" @click="subsub" action="api_login" method="POST"
+                    <form id="login_form" @submit.prevent="" @click="subsub" enctype="multipart/form-data" method="POST"
                         class="flex flex-col justify-center">
                         <label class=" m-2 text-white font-medium">image</label>
                         <div class="container mx-auto h-full flex flex-col justify-center items-center">
-                            <input @change="processFile($event)" type="file" id="multi-upload-input" multiple />
+                            <input @change="processFile($event)" type="file" accept=".png,.jpg,.jpeg"
+                                :maxFileSize="1048576000000" id="multi-upload-input" multiple />
                         </div>
                         <div class="col-span-6 sm:col-span-4">
                             <label class=" m-2 block text-white font-medium text-gray-700">Type</label>
@@ -50,7 +51,7 @@
                                         d="M7 2a1 1 0 012 0v1h2V2a1 1 0 112 0v1h2a2 2 0 012 2v2h1a1 1 0 110 2h-1v2h1a1 1 0 110 2h-1v2a2 2 0 01-2 2h-2v1a1 1 0 11-2 0v-1H9v1a1 1 0 11-2 0v-1H5a2 2 0 01-2-2v-2H2a1 1 0 110-2h1V9H2a1 1 0 010-2h1V5a2 2 0 012-2h2V2zM5 5h10v10H5V5z"
                                         clip-rule="evenodd" />
                                 </svg>
-                                <span class="font-bold text-gray-900">$5 / Prix</span>
+                                <span class="font-bold text-gray-900">$dollar / Prix</span>
                             </div>
                             <div class="flex">
                                 <button type="button" @click="minus()" class="bg-yellow-600 p-1.5 font-bold rounded">
@@ -89,7 +90,7 @@
           focus:invalid:border-red-500 focus:invalid:ring-red-500" type="text" name="username" placeholder="wahyusa">
 
                         <label class="text-white font-medium">description</label>
-                        <textarea class="
+                        <textarea v-model="newPost.description" class="
           mb-3 mt-1 block w-full px-2 py-1.5 border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400
           focus:outline-none
           focus:border-sky-500
@@ -97,12 +98,9 @@
           focus:ring-sky-500
           focus:invalid:border-red-500 focus:invalid:ring-red-500" name="messages"
                             placeholder="Write something"></textarea>
-                        <button
-                            class="px-4 py-1.5 rounded-md shadow-lg bg-gradient-to-r from-pink-600 to-red-600 font-medium text-gray-100 block transition duration-300"
-                            type="submit">
-                            <span id="login_process_state" class="hidden">Sending :)</span>
-                            <span id="login_default_state">Donate<span id="subtotal"></span></span>
-                        </button>
+                        <input id="login_process_state"
+                            class="px-4 py-1.5 hover:cursor-pointer rounded-md shadow-lg bg-gradient-to-r from-pink-600 to-red-600 font-medium text-gray-100 block transition duration-300"
+                            type="submit" value="Sending :)">
                     </form>
                 </div>
             </section>
@@ -112,12 +110,18 @@
 
 
 
-<script>
+<script>import axios from "axios";
 export default {
     name: "add-item",
     methods: {
         processFile(event) {
-            this.newPost.pucture = event.target.files[0]
+            this.newPost.pucture = event.target.files[0];
+            this.url = URL.createObjectURL(this.newPost.pucture);
+            console.log(this.newPost.pucture);
+            let formData = new FormData();
+            formData.append('image', this.newPost.pucture);
+            this.AjouterPost(formData);
+
         },
         plus() {
             let count = parseInt(document.getElementById('item_count').value);
@@ -131,35 +135,25 @@ export default {
                 this.subtotal = count - 1;
             }
         },
-        subsub() {
-            document.getElementById("login_form").onsubmit = function () {
-                event.preventDefault()
-                // animation
-                document.getElementById("login_process_state").classList.remove("hidden")
-                document.getElementById("login_process_state").classList.add("animate-pulse")
-
-                document.getElementById("login_default_state").classList.add("hidden")
-            }
-
-            parseInt(document.getElementById("item_count").value)
-            this.subtotal = parseInt(5)
-        },
         typefun() {
             document.getElementById("vehicle_id").value == "Offre" ? this.offre = true : this.offre = false
         },
-
+        AjouterPost() {
+            axios.post(" http://127.0.0.1:8000/api/AddPost", this.newPost).then(res => {
+                console.log(res);
+            })
+        }
     },
     data() {
         return {
             newPost: {
-                Prix: 1,
+                Prix: 0,
                 Title: '',
                 pucture: '',
                 Type: '',
             },
             offre: false,
-
-
+            url: '',
         }
     },
 
