c67188b466bac3d798e71860c3a9d99601bfc855
BENJY33X <92626379+BENJY33X@users.noreply.github.com>
2022-04-20 13:47:01 +0000

    .

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,25 @@
+import { createApp } from "vue";
+import router from "./router/index";
+import App from "./App.vue";
+import Vue3Lottie from "vue3-lottie";
+
+import * as firebase from "firebase/app";
+import "firebase/storage"
+// import "firebase/firestore"
+import "firebase/auth"
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyCxmcDKSjysiBrACmr13wTmJ8WH2msekVc",
+    authDomain: "projet1-27e8c.firebaseapp.com",
+    projectId: "projet1-27e8c",
+    storageBucket: "projet1-27e8c.appspot.com",
+    messagingSenderId: "739532038732",
+    appId: "1:739532038732:web:e9bcb08ef6886799464ae9",
+    measurementId: "G-K1LDMLET2Z"
+  };
+ 
+firebase.initializeApp(firebaseConfig);
 
 
-import { createApp } from 'vue'
-import router from './router/index';
-import App from './App.vue'
-import Vue3Lottie from 'vue3-lottie'
 
-createApp(App).use(router).use(Vue3Lottie).mount('#app')
+createApp(App).use(router).use(Vue3Lottie).mount("#app");
