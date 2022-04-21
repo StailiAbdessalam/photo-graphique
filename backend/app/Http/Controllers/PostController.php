0c5f67bb60a47a2238ff406d41bfb7c5963deaea
BENJY33X <92626379+BENJY33X@users.noreply.github.com>
2022-04-21 14:58:54 +0000

    post controller delete

--- backend/app/Http/Controllers/PostController.php
+++ backend/app/Http/Controllers/PostController.php
@@ -12,21 +12,34 @@ class PostController extends Controller
     {
         return  json_encode(Post::all());
     }
-
-
     public function Add(Request $request)
     {
 
-            $Post = new Post();
-            $Post->user_id = $request->user_id;
-            $Post->Title = $request->Title;
-            $Post->image = $request->image;
-            $Post->Prix = $request->Prix;
-            $Post->Type = $request->Type;
-            $Post->description = $request->description;
-            $result = $Post->save();
-            echo json_encode($result);
-            // return Post::create($request->all());
-
+        $Post = new Post();
+        $Post->user_id = $request->user_id;
+        $Post->Title = $request->Title;
+        $Post->image = $request->image;
+        $Post->Prix = $request->Prix;
+        $Post->Type = $request->Type;
+        $Post->description = $request->description;
+        $result = $Post->save();
+        echo json_encode($result);
     }
+    public function delete($id)
+    {
+        Post::destroy($id);
+    }
+
+    public function UpdatePost(Request $request , $id){
+        $Post = Post::find($id);
+        $Post->user_id = $request->user_id;
+        $Post->Title = $request->Title;
+        $Post->image = $request->image;
+        $Post->Prix = $request->Prix;
+        $Post->Type = $request->Type;
+        $Post->description = $request->description;
+        $result = $Post->save();
+        echo json_encode($result);
+    }
+
 }
