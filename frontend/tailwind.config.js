593f5da6496f33d348e8399d41e39c11a0a53d0e
BENJY33X <92626379+BENJY33X@users.noreply.github.com>
2022-04-21 14:59:27 +0000

    tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,9 @@
+
+
 module.exports = {
   content: [],
   theme: {
     extend: {},
   },
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}']
-}
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+};
